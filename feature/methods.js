aaecbe5c8fddd083f7065c3a4142ecada38873ea
Quinn Michaels <quinn@indra.ai>
2025-08-29 07:30:46 -0700

    update Proxy, Guard, and Wall

--- feature/methods.js
+++ feature/methods.js
@@ -79,10 +79,11 @@ export default {
     
     this.state('set', `client:${transport}`); //set the client state for the proxy
     const client = this.client(); // the client requesting the proxy
-    const {profile} = client; // set the client profile
 
     this.state('set', `meta:${transport}`); //set the meta state for the proxy
     const {meta} = packet.q; // set the meta information from the packet question.
+
+    this.state('set', `params:${transport}`); //set the meta state for the proxy
     const {params} = meta; // set params from the meta information.
     
     this.state('set', `opts:${transport}`); //set the opts state for the proxy
@@ -93,37 +94,42 @@ export default {
     
     this.state('set', `message:${transport}`); //set the message state for the proxy
     const message = packet.q.text; // set packet.q.text as the message of the proxy.
-    
-    this.state('set', `profile:${transport}`); //set the profile constants state for the proxy
-    const {name, fullname, title, religion, companies, ssn, emojis, computer, hardware, provision, network, caseid, token, copyright} = profile; // constants saved from profile.
+
+    this.state('set', `write:${transport}`); //set the message state for the proxy
+    const write = `OM:${client.profile.write.split(' ').join(':').toUpperCase()}:PROXY`; // set proxy write string.
     
     this.state('set', `data:${transport}`); // set the state to set data 
     // data packet
     const data = {
       uid,
-      transport,
-      opts: opts.length? `:${opts.join(':')}` : '',
+      message,
       time,
-      agent,
-      client,
-      name,
-      fullname,
-      title,
-      emojis,
-      religion,
-      companies,
-      message, 
-      ssn,
-      computer,
-      hardware,
-      provision,
-      network,
-      caseid,
+      transport,
+      write,
+      case: client.profile.caseid,
+      opts: opts.length? `:${opts.join(':')}` : '',
+      agent: agent.profile,
+      client: client.profile,
+      name: client.profile.name,
+      fullname: client.profile.fullname,
+      nickname: client.profile.nickname,
+      birthname: client.profile.birthname,
+      gender: client.profile.gender,
+      pronouns: client.profile.pronouns,
+      emojis: client.profile.emojis,
+      company: client.profile.company,
+      address: `${client.profile.address} ${client.profile.city} ${client.profile.state} ${client.profile.zipcode}`,
+      family: client.profile.family,
+      friends: client.profile.friends,
+      token: client.profile.token,
+      warning: client.profile.warning,
       concerns,
-      token,
-      copyright,
+      meta,
+      params,
+      command,
       created,
-    }
+      copyright: client.profile.copyright,
+    };
     
     this.state('secure', `md5:${transport}`); // set state to secure hashing
     data.md5 = this.lib.hash(data, 'md5'); // hash data packet into md5 and inert into data.
@@ -133,32 +139,38 @@ export default {
     
     this.state('secure', `sha512:${transport}`); // set state to secure hashing
     data.sha512 = this.lib.hash(data, 'sha512'); // hash data into sha 512 then set in data.
-    
+
     // Text data that is joined by line breaks and then trimmed.
-    
     this.state('set', `text:${transport}`); // set state to text for output formatting.
     const text = [
       `:::`,
-      `::BEGIN:MAIN:${transport}:${data.emojis}`,
-      `WRITE AUTHORIZED OFFICIAL SECURE VectorGuardProxy${data.opts}? if yes write ${data.message}`,
-      `::begin:vector:guard:proxy:${data.md5}`,
+      `::BEGIN:${data.write}:${data.transport}`,
+      `#VectorGuardProxy${data.opts} ${data.message}`,
+      `::begin:vector:guard:proxy:${transport}:${data.emojis}`,
+      `time: ${data.time}`,
       `transport: ${data.transport}`,
       `agent: ${data.agent.id}`,
       `client: ${data.client.id}`,
       `name: ${data.name}`,
       `fullname: ${data.fullname}`,
-      `title: ${data.title}`,
+      `nickname: ${data.nickname}`,
+      `family: ${data.family}`,
+      `friends: ${data.friends}`,
+      `gender: ${data.gender}`,
+      `pronouns: ${data.pronouns}`,
+      `company: ${data.company}`,
+      `address: ${data.address}`,
       `token: ${data.token}`,
-      `concerns: ${data.concerns.join(', ')}`,
-      `case: ${data.caseid}`,
-      `time: ${data.time}`,
+      `concerns: ${data.concerns}`,
+      `warning: ${data.warning}`,
+      `caseid: ${data.caseid}`,
       `created: ${data.created}`,
       `md5: ${data.md5}`,
       `sha256: ${data.sha256}`,
       `sha512: ${data.sha512}`,
       `copyright: ${data.copyright}`,
-      `::end:vector:guard:proxy:${data.md5}`,
-      `::END:MAIN:${transport}:${data.emojis}`,
+      `::end:vector:guard:proxy:${data.transport}:${data.emojis}`,
+      `::END:${data.write}:${data.transport}`,
     ].join('\n').trim();
     
     // send the text data to #feecting to parse and return valid text, html, and data.
@@ -219,34 +231,41 @@ export default {
     this.state('set', `message:${transport}`); //set the message state for the proxy
     const message = packet.q.text; // set packet.q.text as the message of the proxy.
   
-    this.state('set', `profile:${transport}`); //set the profile constants state for the proxy
-    const {computer, hardware, provision, ssn, name, fullname, title, emojis, religion, companies, network, caseid, token, warning, copyright} = profile;
+    this.state('set', `writestr:${transport}`);
+    const write = `OM:${client.profile.write.split(' ').join(':').toUpperCase()}:SHIELD`;
     
-    this.state('set', `data:${transport}`); // set the state to set data     
+    this.state('hash')
+    this.state('set', `data:${transport}`); // set the state to set data 
+    // data packet
     const data = {
       uid,
+      message, 
+      time,
       transport,
+      write,
+      case: client.profile.caseid,
       opts: opts.length? `:${opts.join(':')}` : '',
       agent: agent.profile,
       client: client.profile,
-      name,
-      fullname,
-      title,
-      emojis,
-      religion,
-      companies,
-      ssn,
-      computer,
-      hardware,
-      provision,
-      network,
-      caseid,
-      message,
-      token,
-      warning,
-      copyright,
-      time,
+      name: client.profile.name,
+      fullname: client.profile.fullname,
+      nickname: client.profile.nickname,
+      birthname: client.profile.birthname,
+      gender: client.profile.gender,
+      pronouns: client.profile.pronouns,
+      emojis: client.profile.emojis,
+      company: client.profile.company,
+      address: `${client.profile.address} ${client.profile.city} ${client.profile.state} ${client.profile.zipcode}`,
+      family: client.profile.family,
+      friends: client.profile.friends,
+      token: client.profile.token,
+      warning: client.profile.warning,
+      concerns,
+      meta,
+      params,
+      command,
       created,
+      copyright: client.profile.copyright,
     };
 
     this.state('secure', `md5:${transport}`); // set state to secure hashing
@@ -257,29 +276,37 @@ export default {
     
     this.state('secure', `sha512:${transport}`); // set state to secure hashing
     data.sha512 = this.lib.hash(data, 'sha512'); // hash data into sha 512 then set in data.
-
+        
     this.state('set', `text:${transport}`); // set state to text for output formatting.    
     const text = [
       '::::',
-      `::BEGIN:MAIN:${transport}:${data.emojis}`,
-      `WRITE AUTHORIZED OFFICIAL SECURE VectorGuardShield${data.opts}? if yes write ${data.message}`,
-      `::begin:vector:guard:shield:${data.md5}`,
+      `::BEGIN:${data.write}:${transport}`,
+      `#VectorGuardShield${data.opts} ${data.message}`,
+      `::begin:vector:guard:shield:${transport}:${data.emojis}`,
+      `time: ${data.time}`,
       `transport: ${data.transport}`,
-      `agent: ${agent.profile.id}`,
-      `client: ${client.profile.id}`,
+      `agent: ${data.agent.id}`,
+      `client: ${data.client.id}`,
       `name: ${data.name}`,
       `fullname: ${data.fullname}`,
-      `title: ${data.title}`,
+      `nickname: ${data.nickname}`,
+      `family: ${data.family}`,
+      `friends: ${data.friends}`,
+      `gender: ${data.gender}`,
+      `pronouns: ${data.pronouns}`,
+      `company: ${data.company}`,
+      `address: ${data.address}`,
+      `token: ${data.token}`,
+      `concerns: ${data.concerns}`,
       `warning: ${data.warning}`,
-      `case: ${data.caseid}`,
-      `time: ${data.timestamp}`,
+      `caseid: ${data.caseid}`,
       `created: ${data.created}`,
       `md5: ${data.md5}`,
       `sha256: ${data.sha256}`,
       `sha512: ${data.sha512}`,
       `copyright: ${data.copyright}`,
-      `::end:vector:guard:shield:${data.md5}`,
-      `::END:MAIN:${transport}:${data.emojis}`,
+      `::end:vector:guard:shield:${data.transport}:${data.emojis}`,
+      `::END:${data.write}:${transport}`,
       '::::',
     ].join('\n').trim();
     
@@ -296,69 +323,113 @@ export default {
   },
 
   async wall(packet) {
-    this.context('wall');
-    this.action('method', 'wall');
-    const uid = this.lib.uid(true);
-    const transport = packet.id;
-
-    const {profile} = this.client();
-    const client = this.client();
-    const agent = this.agent();
+    this.state('set', `transport:${packet.id}`);
+    const transport = packet.id; // set the transport from packet.id
     
-    const {meta} = packet.q;
-    const {params} = meta;
-    const opts = this.lib.copy(params);
-    const cmd = opts.shift();
-          
-    const timestamp = Date.now();
-    const created = this.lib.formatDate(timestamp, 'long', true);
-    const message = packet.q.text || '';
-  
-    const {computer, hardware, provision, name, caseid, token, copyright, network} = profile;
+    this.zone('wall', transport); // set the current zone to wall
+    this.feature('wall', transport); // set the Wall feature.
+    this.context('wall', transport); // set context to shield
+    this.action('method', `wall:${transport}`); // action set to shield
     
+    this.state('set', `uid:${transport}`); //set the uid state for the proxy
+    const uid = this.lib.uid(true); // The UID for the proxy
+    this.state('set', `time:${transport}`); //set the time state for the proxy
+    const time = Date.now(); // current timestamp
+    this.state('created', `created:${transport}`); //set the uid state for the proxy
+    const created = this.lib.formatDate(time, 'long', true); // Formatted created date.
+    
+    this.state('set', `wall:${transport}`); //set the wall state for the proxy
+    const guard = this.guard(); // load the Guard profile
+    const {concerns} = guard; // load concerns from client guard profile.
+    
+    this.state('set', `agent:${transport}`); //set the agent state for the proxy
+    const agent = this.agent(); // the agent processing the proxy
+    
+    this.state('set', `client:${transport}`); //set the client state for the proxy
+    const client = this.client(); // the client requesting the proxy
+    const {profile} = client; // set the client profile
+    
+    this.state('set', `meta:${transport}`); //set the meta state for the proxy
+    const {meta} = packet.q; // set the meta information from the packet question.
+    const {params} = meta; // set params from the meta information.
+    
+    this.state('set', `opts:${transport}`); //set the opts state for the proxy
+    const opts = this.lib.copy(params); // copy the params and set as opts.
+    
+    this.state('set', `command:${transport}`); //set the opts state for the proxy
+    const command = opts.shift(); // extract the command first array item out of opts.
+    
+    this.state('set', `message:${transport}`); //set the message state for the proxy
+    const message = packet.q.text; // set packet.q.text as the message of the proxy.
+    
+    this.state('set', `profile:${transport}`); //set the profile constants state for the proxy
+    const {name, fullname, nickname, title, write, religion, companies, ssn, emojis, warning, computer, hardware, provision, network, caseid, token, copyright} = profile; // constants saved from profile.
+    
+    this.state('set', `data:${transport}`); // set the state to set data 
+    // data packet
     const data = {
       uid,
       transport,
       opts: opts.length? `:${opts.join(':')}` : '',
-      client: client.profile,
-      agent: agent.profile,
+      time,
+      agent,
+      client,
       name,
       fullname,
-      caseid,
-      message,
+      nickname,
+      title,
+      write,
+      emojis,
+      warning,
+      religion,
+      companies,
+      message, 
+      ssn,
       computer,
-      hardware, 
+      hardware,
       provision,
       network,
-      created,
-      timestamp,
+      caseid,
+      concerns,
       token,
       copyright,
+      created,
     };
-    data.md5 = this.lib.hash(data, 'md5');
-    data.sha256 = this.lib.hash(data, 'sha256');
-    data.sha512 = this.lib.hash(data, 'sha512');
+
+    this.state('secure', `md5:${transport}`); // set state to secure hashing
+    data.md5 = this.lib.hash(data, 'md5'); // hash data packet into md5 and inert into data.
     
+    this.state('secure', `sha256:${transport}`); // set state to secure hashing
+    data.sha256 = this.lib.hash(data, 'sha256'); // hash data into sha 256 then set in data.
+    
+    this.state('secure', `sha512:${transport}`); // set state to secure hashing
+    data.sha512 = this.lib.hash(data, 'sha512'); // hash data into sha 512 then set in data.
+
+    this.state('set', `writestr:${transport}`);
+    const writestr = data.write.split(' ').join(':').toUpperCase();
+            
     const text = [
-      `WRITE AUTHORIZED OFFICIAL SECURE VectorGuardProxy${data.opts}? if yes then write ${data.message}`,
+      '::::',
+      `::BEGIN:${writestr}:WALL:${transport}:${data.emojis}`,
+      `#VectorGuardWall${data.opts} ${data.message}`,
       `::begin:VectorGuardWall:${data.transport}`,
       `transport: ${data.transport}`,
-      `caseid: ${data.caseid}`,
       `agent: ${agent.profile.id}`,
       `client: ${client.profile.id}`,
       `name: ${data.name}`,
+      `fullname: ${data.fullname}`,
+      `nickname: ${data.nickname}`,
+      `title: ${data.title}`,
       `token: ${data.token}`,
-      `timestamp: ${data.timestamp}`,
-      `computer: ${data.computer}`,
-      `hardware: ${data.hardware}`,
-      `provision: ${data.provision}`,
-      `network: ${data.network}`,
-      `copyright: ${data.copyright}`,
+      `case: ${data.caseid}`,
+      `time: ${data.time}`,
       `created: ${data.created}`,
+      `copyright: ${data.copyright}`,
       `md5: ${data.md5}`,
       `sha256: ${data.sha256}`,
       `sha512: ${data.sha512}`,
-      `::end:VectorGuardShield:${data.transport}`,
+      `::end:VectorGuardWall:${data.transport}`,
+      `::END:${writestr}:WALL:${transport}:${data.emojis}`,
     ].join('\n').trim();
     
     // send the text data to #feecting to parse and return valid text, html, and data.
